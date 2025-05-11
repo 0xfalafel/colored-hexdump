a59f6fc3ce4ca34984889b201cc04f8c9664010b
Lasne Olivier <lasneolivier@gmail.com>
2025-05-11 16:57:28 +0200

    colorize a every bytes

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use colored::Colorize;
 
-const RESET: &str = "\x1b[0m";
-const LIGHT_GREY: &str = "\x1b[38┊;5;242m";
+const RESET: &str   = "\x1b[0m";
+const LIGHT_GREY: &str = "\x1b[242m";
+const GREEN:   &str = "\x1b[32m";
+const YELLOW:  &str = "\x1b[33m";
+const MAGENTA: &str = "\x1b[35m";
+const CYAN:    &str = "\x1b[36m";
 
 pub fn hexdump(bytes: &[u8]) -> String {
     let mut output = String::new();
@@ -15,9 +19,11 @@ pub fn hexdump(bytes: &[u8]) -> String {
 
 fn colorize_byte(byte: &u8) -> String {
     let color = match byte {
-        0x00 => LIGHT_GREY,
-
-        _ => todo!()
+        0x00 => LIGHT_GREY, // null bytes
+        b'\t' | b'\n' | 0x0c | b'\r' | b' ' => GREEN, // ascii whitespace
+        0x21..0x7f => CYAN, // printable ascii
+        ..0x80 => MAGENTA,  // non-printable ascii
+        _ => YELLOW,        // other
     };
     format!("{}{:02x}{}", color, byte, RESET)
 }
